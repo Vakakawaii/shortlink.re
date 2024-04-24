7c93312217c8dc9dadd7dec006756bc03a6820e9
Vakakawaii <vakasankawaii@gmail.com>
2024-04-24 21:44:39 +0800

    1.1 :返回实体+异常码+异常拦截器

--- admin/src/main/java/org/vakakawaii/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/vakakawaii/shortlink/admin/controller/UserController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.vakakawaii.shortlink.admin.common.convention.result.Result;
+import org.vakakawaii.shortlink.admin.common.convention.result.Results;
+import org.vakakawaii.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.vakakawaii.shortlink.admin.dto.resp.UserRespDTO;
 import org.vakakawaii.shortlink.admin.service.UserService;
 
@@ -17,10 +20,9 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username")String username){
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username")String username){
+        return Results.success(userService.getUserByUsername(username));
     }
-
 
 
 }
--- admin/src/main/java/org/vakakawaii/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/vakakawaii/shortlink/admin/service/impl/UserServiceImpl.java
@@ -5,6 +5,10 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.vakakawaii.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+import org.vakakawaii.shortlink.admin.common.convention.errorcode.IErrorCode;
+import org.vakakawaii.shortlink.admin.common.convention.exception.ClientException;
+import org.vakakawaii.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.vakakawaii.shortlink.admin.dao.entity.UserDO;
 import org.vakakawaii.shortlink.admin.dao.mapper.UserMapper;
 import org.vakakawaii.shortlink.admin.dto.resp.UserRespDTO;
@@ -24,6 +28,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername,username);
         UserDO userDO  = baseMapper.selectOne(queryWrapper);
+
+        if(userDO == null){
+            throw new ClientException(UserErrorCodeEnum.USER_NULL);
+        }
+
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
 
