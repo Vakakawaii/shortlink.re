42d0332ffd37cf9d019bd854a7471597056980f1
Vakakawaii <vakasankawaii@gmail.com>
2024-05-14 15:29:17 +0800

    3.4 :link表分库

--- admin/src/test/java/org/vakakawaii/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/org/vakakawaii/shortlink/admin/test/UserTableShardingTest.java
@@ -2,20 +2,25 @@ package org.vakakawaii.shortlink.admin.test;
 
 public class UserTableShardingTest {
 
-    private static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
-            "`id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint DEFAULT NULL COMMENT '注销时间戳',\n" +
+    private static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
+            "  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '分组标识',\n" +
+            "  `domain` varchar(128) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '原始链接',\n" +
+            "  `click_num` int DEFAULT NULL COMMENT '点击量',\n" +
+            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0:启用 1:未启用',\n" +
+            "  `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0:接口创建 1:控制台创建',\n" +
+            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0:永久有效 1:自定义',\n" +
+            "  `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "  `describe` varchar(1024) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE COMMENT '用户名不可重复'\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1783799197099741187 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+            "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1790215054239559682 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
