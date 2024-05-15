e8e5924208336825d5ce01a63992624650dabbbd
Vakakawaii <vakasankawaii@gmail.com>
2024-05-15 15:48:33 +0800

    3.7 :加入遗漏的分页响应参数的变量

--- project/src/main/java/org/vakakawaii/shortlink/project/dto/resp/LinkPageRespDTO.java
+++ project/src/main/java/org/vakakawaii/shortlink/project/dto/resp/LinkPageRespDTO.java
@@ -1,5 +1,8 @@
 package org.vakakawaii.shortlink.project.dto.resp;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -48,6 +51,7 @@ public class LinkPageRespDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
@@ -59,4 +63,12 @@ public class LinkPageRespDTO {
      *  图标
      */
     private String favicon;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+
 }
